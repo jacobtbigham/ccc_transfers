26c4fe22397c853358ab69465e5a8c0cdad8607f
jacobtbigham <60991562+jacobtbigham@users.noreply.github.com>
2021-08-25 04:07:23 -0700

    Update databasemaker.py
    Now we can construct a DatabaseMaker with an id_to_key dictionary so that the final JSON file has the key value for each agreement stored with each requirement. (This is for front-end: we want each course equivalency to know where it came from.)

--- databasemaker.py
+++ databasemaker.py
@@ -1,27 +1,26 @@
 import os
 import urllib.request
-import json
-from pdfextractor import PDFExtractor
+import json 
 from collections import defaultdict
 
-
 class DatabaseMaker():
-    def __init__(self, school_name, major_code):
+    def __init__(self, school_name, major_code, id_to_key):
         self.database = defaultdict(list)
         with urllib.request.urlopen("https://assist.org/api/institutions") as url:
             data = json.loads(url.read().decode())
-        self.names = {x['id']: x['names'][0]['name'] for x in list(data)}
+        self.names = {x['id']: x['names'][0]['name'] for x in list(data)} 
         self.major_code = major_code
         self.school_name = school_name
-
+        self.id_to_key = id_to_key
+        
     def alphabetize_class_dict(self, class_dict):
         sorted_classes = sorted(class_dict.keys())
         class_dict = {key: class_dict[key] for key in sorted_classes}
         for key in class_dict.keys():
-            sorted_schools = sorted(class_dict[key], key=lambda x: x['from_school'])
+            sorted_schools = sorted(class_dict[key], key=lambda x : x['from_school'])
             class_dict[key] = sorted_schools
         return class_dict
-
+    
     def add_classes(self):
         for file_name in os.listdir('agreements/'):
             if self.major_code not in file_name or 'report' not in file_name:
@@ -29,14 +28,15 @@ class DatabaseMaker():
             info = file_name.replace('.pdf', '').split('_')
             to_school_id = info[1]
             from_school_id = int(info[2])
-            extractor = PDFExtractor(f'agreements/{file_name}')
+            extractor = PDF_Extractor(f'agreements/{file_name}')
             classes = extractor.dict_from_file()
             for to_class in classes.keys():
                 from_class = {'school_id': from_school_id,
                               'from_school': self.names[from_school_id],
-                              'equiv': classes[to_class]}
+                              'equiv': classes[to_class],
+                              'key': id_to_key[from_school_id]}
                 self.database[to_class].append(from_class)
         self.database = self.alphabetize_class_dict(self.database)
         json_name = f'agreements/{self.school_name}/{self.major_code}.json'
-        with open(json_name, "w") as out_file:
+        with open(json_name, "w") as out_file: 
             json.dump(self.database, out_file, indent=4)
