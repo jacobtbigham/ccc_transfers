104da6bf9965e1aec2722f9caf87005ba88b4d14
jacobtbigham <60991562+jacobtbigham@users.noreply.github.com>
2021-08-25 04:09:21 -0700

    Update main.py
    Main functionality now incorporates id_to_key intermediary. Perhaps not an ideal fix... but the interface for running this script isn't the most important aspect of the project, and anyone running it is just going to copy and paste anyways.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from databasemaker import DatabaseMaker
 
 def main():
     grabber = PDFGrabber(120, 'Software Engineering, B.S.', 'SWE', 0.2)
-    grabber.get_pdfs()
-    maker = DatabaseMaker('UCI', 'SWE')
+    id_to_key = grabber.get_pdfs()
+    maker = DatabaseMaker('UCI', 'SWE', id_to_key)
     maker.add_classes()
 
 
